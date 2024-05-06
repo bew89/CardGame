3ce4476b92a6d342f0df5a52db46288935b15d70
Ben <benwoodfield21@icloud.com>
2024-05-06 22:42:18 +0100

    Update CardFunctionality.js
    random cards and added click feature

--- CardFunctionality.js
+++ CardFunctionality.js
@@ -10,67 +10,67 @@ const cardsData = {
 
     clubs: [
         //Club images
-        {id: 1, value: 'A', imagePath: 'cards/1_club.png'},
-        {id: 2, value: '2', imagePath: 'cards/2_club.png'},
-        {id: 3, value: '3', imagePath: 'cards/3_club.png'},
-        {id: 4, value: '4', imagePath: 'cards/4_club.png'},
-        {id: 5, value: '5', imagePath: 'cards/5_club.png'},
-        {id: 6, value: '6', imagePath: 'cards/6_club.png'},
-        {id: 7, value: '7', imagePath: 'cards/7_club.png'},
-        {id: 8, value: '8', imagePath: 'cards/8_club.png'},
-        {id: 9, value: '9', imagePath: 'cards/9_club.png'},
-        {id: 10, value: '10', imagePath: 'cards/10_club.png'},
-        {id: 11, value: 'J', imagePath: 'cards/11_club.png'},
-        {id: 12, value: 'Q', imagePath: 'cards/12_club.png'},
-        {id: 13, value: 'K', imagePath: 'cards/13_club.png'},
+        {id: 1, inUse: false,value: 'A', imagePath: 'cards/1_club.png'},
+        {id: 2, inUse: false,value: '2', imagePath: 'cards/2_club.png'},
+        {id: 3, inUse: false,value: '3', imagePath: 'cards/3_club.png'},
+        {id: 4, inUse: false,value: '4', imagePath: 'cards/4_club.png'},
+        {id: 5, inUse: false,value: '5', imagePath: 'cards/5_club.png'},
+        {id: 6, inUse: false,value: '6', imagePath: 'cards/6_club.png'},
+        {id: 7, inUse: false,value: '7', imagePath: 'cards/7_club.png'},
+        {id: 8, inUse: false,value: '8', imagePath: 'cards/8_club.png'},
+        {id: 9, inUse: false,value: '9', imagePath: 'cards/9_club.png'},
+        {id: 10, inUse: false,value: '10', imagePath: 'cards/10_club.png'},
+        {id: 11, inUse: false,value: 'J', imagePath: 'cards/11_club.png'},
+        {id: 12, inUse: false,value: 'Q', imagePath: 'cards/12_club.png'},
+        {id: 13, inUse: false,value: 'K', imagePath: 'cards/13_club.png'},
     ],
     diamonds: [
         //Diamond images
-        {id: 14, value: 'A', imagePath: 'cards/1_diamond.png'},
-        {id: 15, value: '2', imagePath: 'cards/2_diamond.png'},
-        {id: 16, value: '3', imagePath: 'cards/3_diamond.png'},
-        {id: 17, value: '4', imagePath: 'cards/4_diamond.png'},
-        {id: 18, value: '5', imagePath: 'cards/5_diamond.png'},
-        {id: 19, value: '6', imagePath: 'cards/6_diamond.png'},
-        {id: 20, value: '7', imagePath: 'cards/7_diamond.png'},
-        {id: 21, value: '8', imagePath: 'cards/8_diamond.png'},
-        {id: 22, value: '9', imagePath: 'cards/9_diamond.png'},
-        {id: 23, value: '10', imagePath: 'cards/10_diamond.png'},
-        {id: 24, value: 'J', imagePath: 'cards/11_diamond.png'},
-        {id: 25, value: 'Q', imagePath: 'cards/12_diamond.png'},
-        {id: 26, value: 'K', imagePath: 'cards/13_diamond.png'},
+        {id: 14, inUse: false,value: 'A', imagePath: 'cards/1_diamond.png'},
+        {id: 15, inUse: false,value: '2', imagePath: 'cards/2_diamond.png'},
+        {id: 16, inUse: false,value: '3', imagePath: 'cards/3_diamond.png'},
+        {id: 17, inUse: false,value: '4', imagePath: 'cards/4_diamond.png'},
+        {id: 18, inUse: false,value: '5', imagePath: 'cards/5_diamond.png'},
+        {id: 19, inUse: false,value: '6', imagePath: 'cards/6_diamond.png'},
+        {id: 20, inUse: false,value: '7', imagePath: 'cards/7_diamond.png'},
+        {id: 21, inUse: false,value: '8', imagePath: 'cards/8_diamond.png'},
+        {id: 22, inUse: false,value: '9', imagePath: 'cards/9_diamond.png'},
+        {id: 23, inUse: false,value: '10', imagePath: 'cards/10_diamond.png'},
+        {id: 24, inUse: false,value: 'J', imagePath: 'cards/11_diamond.png'},
+        {id: 25, inUse: false,value: 'Q', imagePath: 'cards/12_diamond.png'},
+        {id: 26, inUse: false,value: 'K', imagePath: 'cards/13_diamond.png'},
     ],
     hearts: [
         //Heart images
-        {id: 27, value: 'A', imagePath: 'cards/1_heart.png'},
-        {id: 28, value: '2', imagePath: 'cards/2_heart.png'},
-        {id: 29, value: '3', imagePath: 'cards/3_heart.png'},
-        {id: 30, value: '4', imagePath: 'cards/4_heart.png'},
-        {id: 31, value: '5', imagePath: 'cards/5_heart.png'},
-        {id: 32, value: '6', imagePath: 'cards/6_heart.png'},
-        {id: 33, value: '7', imagePath: 'cards/7_heart.png'},
-        {id: 34, value: '8', imagePath: 'cards/8_heart.png'},
-        {id: 35, value: '9', imagePath: 'cards/9_heart.png'},
-        {id: 36, value: '10', imagePath: 'cards/10_heart.png'},
-        {id: 37, value: 'J', imagePath: 'cards/11_heart.png'},
-        {id: 38, value: 'Q', imagePath: 'cards/12_heart.png'},
-        {id: 39, value: 'K', imagePath: 'cards/13_heart.png'},
+        {id: 27, inUse: false,value: 'A', imagePath: 'cards/1_heart.png'},
+        {id: 28, inUse: false,value: '2', imagePath: 'cards/2_heart.png'},
+        {id: 29, inUse: false,value: '3', imagePath: 'cards/3_heart.png'},
+        {id: 30, inUse: false,value: '4', imagePath: 'cards/4_heart.png'},
+        {id: 31, inUse: false,value: '5', imagePath: 'cards/5_heart.png'},
+        {id: 32, inUse: false,value: '6', imagePath: 'cards/6_heart.png'},
+        {id: 33, inUse: false,value: '7', imagePath: 'cards/7_heart.png'},
+        {id: 34, inUse: false,value: '8', imagePath: 'cards/8_heart.png'},
+        {id: 35, inUse: false,value: '9', imagePath: 'cards/9_heart.png'},
+        {id: 36, inUse: false,value: '10', imagePath: 'cards/10_heart.png'},
+        {id: 37, inUse: false,value: 'J', imagePath: 'cards/11_heart.png'},
+        {id: 38, inUse: false,value: 'Q', imagePath: 'cards/12_heart.png'},
+        {id: 39, inUse: false,value: 'K', imagePath: 'cards/13_heart.png'},
     ],
     spades: [
         //Spade images.
-        {id: 40, value: 'A', imagePath: 'cards/1_spade.png'},
-        {id: 41, value: '2', imagePath: 'cards/2_spade.png'},
-        {id: 42, value: '3', imagePath: 'cards/3_spade.png'},
-        {id: 43, value: '4', imagePath: 'cards/4_spade.png'},
-        {id: 44, value: '5', imagePath: 'cards/5_spade.png'},
-        {id: 45, value: '6', imagePath: 'cards/6_spade.png'},
-        {id: 46, value: '7', imagePath: 'cards/7_spade.png'},
-        {id: 47, value: '8', imagePath: 'cards/8_spade.png'},
-        {id: 48, value: '9', imagePath: 'cards/9_spade.png'},
-        {id: 49, value: '10', imagePath: 'cards/10_spade.png'},
-        {id: 50, value: 'J', imagePath: 'cards/11_spade.png'},
-        {id: 51, value: 'Q', imagePath: 'cards/12_spade.png'},
-        {id: 52, value: 'K', imagePath: 'cards/13_spade.png'}
+        {id: 40, inUse: false,value: 'A', imagePath: 'cards/1_spade.png'},
+        {id: 41, inUse: false,value: '2', imagePath: 'cards/2_spade.png'},
+        {id: 42, inUse: false,value: '3', imagePath: 'cards/3_spade.png'},
+        {id: 43, inUse: false,value: '4', imagePath: 'cards/4_spade.png'},
+        {id: 44, inUse: false,value: '5', imagePath: 'cards/5_spade.png'},
+        {id: 45, inUse: false,value: '6', imagePath: 'cards/6_spade.png'},
+        {id: 46, inUse: false,value: '7', imagePath: 'cards/7_spade.png'},
+        {id: 47, inUse: false,value: '8', imagePath: 'cards/8_spade.png'},
+        {id: 48, inUse: false,value: '9', imagePath: 'cards/9_spade.png'},
+        {id: 49, inUse: false,value: '10', imagePath: 'cards/10_spade.png'},
+        {id: 50, inUse: false,value: 'J', imagePath: 'cards/11_spade.png'},
+        {id: 51, inUse: false,value: 'Q', imagePath: 'cards/12_spade.png'},
+        {id: 52, inUse: false,value: 'K', imagePath: 'cards/13_spade.png'}
     ]
 }
 
@@ -94,17 +94,21 @@ function dealCards() {
     for (let i = 0; i < 14; i++) {
         let suits = ["clubs", "diamonds", "hearts", "spades"]
         let randomSuit = suits[Math.floor(Math.random() * 4)];
-        let randomCard = Math.floor(Math.random() * 13) + 1;
-
+        let randomCard = Math.floor(Math.random() * 13);// + 1;
 
         let dealtCard = cardsData[randomSuit].at(randomCard);
 
-        if (i % 2 === 0) {
-            usersCards.push(dealtCard);
-        } else {
-            computersCards.push(dealtCard);
+        if(dealtCard.inUse === false) {
+            dealtCard.inUse = true;
+            if (i % 2 === 0) {
+                usersCards.push(dealtCard);
+            } else {
+                computersCards.push(dealtCard);
+            }
+            console.log("Card " + i + ", Suit: " + randomSuit + ", Card: " + dealtCard.id);
+        }else{
+            i--;
         }
-
     }
 }
 
@@ -124,7 +128,11 @@ function displayOpponentsCards() {
     for (let i = 0; i < computersCards.length; i++) {
         const card = computersCards[i];
         const cardImage = document.createElement("img");
-        cardImage.src = card.imagePath;
+        try {
+            cardImage.src = card.imagePath;
+       }catch (e) {
+       console.log(card.id);
+       }
         cardImage.classList.add("card");
         cardImage.alt = card.value;
         opponentSections.appendChild(cardImage);
@@ -139,9 +147,14 @@ function displayUsersCards() {
         cardImage.src = card.imagePath;
         cardImage.classList.add("card");
         cardImage.alt = card.value;
-        // Append the image element to the main section
+        cardImage.addEventListener("click", function() {
+            cardClick(cardImage, card);
+        });
         usersSection.appendChild(cardImage);
     }
+}
+function cardClick(cardImage, card) {
+    console.log("Clicked on card:", card);
 }
 
 
